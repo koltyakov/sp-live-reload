65433f08e29f98cc72def9939cd89aa0b34c5ad4
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2016-11-07 12:27:06 +0300

    Raw path fix

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ spf.liveReload = function(settings) {
     _self.settings.protocol = settings.protocol || (_self.settings.siteUrl.indexOf("https://") !== -1 ? "https" : "http");
 
     _self.emitUpdatedPath = function(filePath, raw) {
-        if (raw) {
+        var raw = (typeof raw === "undefined" ? false : raw);
+        if (!raw) {
             var spRelUrl = (_self.settings.siteUrl + "/" + _self.settings.spFolder.replace(/\\/g, "/"));
             spRelUrl = spRelUrl.replace("://", "").replace(_self.settings.siteUrl.replace("://", "").split("/")[0], "").replace(/\/\//g,'/');
             filePath = filePath.replace(path.join(_self.settings.watchBase), spRelUrl).replace(/\\/g, "/").replace(/\/\//g,'/');
