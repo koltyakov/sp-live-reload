770c00a431c5d9aa35483e23d14ad24884b31234
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2017-08-09 20:13:33 +0300

    Bluebird dependency removed

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import * as Promise from 'bluebird';
 import * as express from 'express';
 
 import Provisioning from './utils/provisioning';
